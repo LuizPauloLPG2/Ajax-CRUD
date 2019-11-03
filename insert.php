46586d757a64ca2582a78f97a4d97eb78c1bd4dd
Luiz Paulo <37484860+luizpaulogroup@users.noreply.github.com>
2019-11-03 20:20:51 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once("./conexao.php");
+
 $nome = $_POST["nome_estudo"];
 $desc = $_POST["desc_estudo"];
+
 $sql = ("insert into tbestudo (nome, descricao) values (upper(:nome), upper(:desc))");
 
-$stmt = Db::_conexao()->prepare($sql);
+$stmt = Db::init()->prepare($sql);
 $stmt->bindValue(":nome", $nome);
 $stmt->bindValue(":desc", $desc);
 $stmt->execute();
