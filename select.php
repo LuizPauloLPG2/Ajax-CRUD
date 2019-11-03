cfacc15cbc221cb6a1ffddd13b571f2a386be028
Luiz Paulo <37484860+luizpaulogroup@users.noreply.github.com>
2019-11-03 20:21:25 -0300

    Update select.php

--- select.php
+++ select.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once ("./conexao.php");
-$id = $_REQUEST['id'];
+
+$id = $_POST['id'];
+
 $sql = ("select * from tbestudo where id_estudo = :id");
-$stmt = Db::_conexao()->prepare($sql);
+
+$stmt = Db::init()->prepare($sql);
 $stmt->bindValue(":id", $id);
 $stmt->execute();
 $stmt = $stmt->fetch(PDO::FETCH_ASSOC);
