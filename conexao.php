3273b20edb88e89afa572f211849f03f3b4dd8b7
Luiz Paulo <37484860+luizpaulogroup@users.noreply.github.com>
2020-03-30 10:34:40 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,7 +6,10 @@ class Db
     public static function init()
     {
         try {
-            return new PDO("mysql:host=localhost;dbname=database;", "root", "");
+            return new PDO("mysql:host=localhost;dbname=database;", "root", "", array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                    ));
         } catch (Exception $e) {
             die($e->getMessage());
         }
